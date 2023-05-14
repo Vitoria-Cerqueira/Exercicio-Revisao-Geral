e3545440d4981a4e614526614bdc7bf96cecf6c1
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-05-13 23:57:57 -0300

    Upando exercicio 1 completos

--- src/ExUm.java
+++ src/ExUm.java
@@ -13,21 +13,23 @@ public class ExUm {
     static Scanner teclado = new Scanner(System.in);
     static List <Double> alturaFeminina = new ArrayList<>();
     static List <Double> alturaMasculina = new ArrayList<>();
-    static Double maiorValor = Double.MIN_VALUE;
-    static Double menorValor = Double.MAX_VALUE;
+    static Double maiorValor = 0.0; // essa maior valor
+    static Double menorValor = 0.0; // essa é pra pegar menor valor
 
     public static void main(String[] args) {
         informacoesUsuario();
-        verificarAlturaFem();
+        verificarAlturaGrupo();
+        verificarMediaAlturaMas();
+        verificarQtaMulheres();
     }
     static double altura;
     public static void informacoesUsuario (){
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Digite sua altura");
             altura = teclado.nextDouble();
 
             System.out.println("Digite seu sexo \nF- Feminino \n M - Masculino");
-            char sexo = teclado.next().charAt(0);
+            char sexo = teclado.next().charAt(0); // pegar o primeiro caracter
 
             if (sexo == 'F' || sexo == 'M'){ // validação do sexo
                 System.out.println(sexo);
@@ -38,35 +40,49 @@ public class ExUm {
 
             if (sexo == 'F'){
                 alturaFeminina.add(altura);
-                System.out.println(alturaFeminina);
+
             }else {
                 alturaMasculina.add(altura);
-                System.out.println(alturaMasculina);
+
             }
         }
-
+        System.out.println("Alturas femininas " + alturaFeminina);
+        System.out.println("Alturas masculinas " + alturaMasculina);
     }
-    public static void verificarAlturaFem (){
-        for (int i = 0; i < alturaFeminina.size(); i++) {
 
-            if (alturaFeminina.get(i) > maiorValor){
+    public static void verificarAlturaGrupo (){
+        List <List<Double>> grupos = new ArrayList<>();
+        grupos.add(alturaFeminina);
+        grupos.add(alturaMasculina);
 
-                maiorValor = alturaFeminina.get(i);
+        for (List<Double> altura:grupos) {
+            for (int i = 0; i < altura.size(); i++) {
+                if (altura.get(i) > maiorValor){
+
+                    maiorValor = altura.get(i);
+                } else if (menorValor == 0 || altura.get(i) < menorValor) {
+                    menorValor = altura.get(i);
+                }
             }
-
-        }
-
-        for (int i = 0; i < alturaFeminina.size(); i++) {
-
-            if (alturaFeminina.get(i) < menorValor){
-
-                menorValor = alturaFeminina.get(i);
-            }
-
         }
         System.out.println("O menor valor é " + menorValor);
         System.out.println("O maior valor é " + maiorValor);
+
     }
+    public static void verificarMediaAlturaMas(){
+        double quantidade =0 ;
+        for (int i = 0; i < alturaMasculina.size(); i++) {
+            quantidade += alturaMasculina.get(i);
+
+        }
+        double media =0.0;
+        media = quantidade / alturaMasculina.size();
+        System.out.printf("A media das alturas masculinas é %.2f" , media);
+    }
+    public static void verificarQtaMulheres (){
+        System.out.println("A quantidade de mulheres é " + alturaFeminina.size());
+    }
+
 
 }
 
